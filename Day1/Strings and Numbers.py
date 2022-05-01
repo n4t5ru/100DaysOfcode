0c04bc10001859efb4c3b04aebced4eb3eed0a5c
n4t5ru <hello@nasru.me>
2022-05-01 21:25:56 +0500

    Day1: Python variables, strings & nums

---
+++ Day1/Strings and Numbers.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+#Below are basic python variables
+#Strings
+str1 = 'Hello World!'
+strinG = 'This is a string'
+
+#Integers
+int1 = 10000
+int2 = 1099
+int3 = 40
+
+#floats
+flt1 = 1000.0
+flt2 = 101.0
+flt3 = 1.5
+
+#Prints various parts of the string
+print(strinG)
+print(strinG[2])
+print(strinG[0:9])
+print(str1 + strinG)
+print(strinG*4)
+
+#Basic arithmetic operations
+print(int1+int2+int3)
+print(int1/int3)
+print(int2-int3)
+print(int1*int3)
---
+++ README.md
@@ -0,0 +1,3 @@
+# #100DaysOfCode Challenge
+Everyday, for the next 100 Days, I will push a python script which does a random task. 
+This shall be my learning progress.
