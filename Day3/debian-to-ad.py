1eafc884a817d54d4a16064f07ff47a3b0959107
n4t5ru <hello@nasru.me>
2022-05-04 12:08:07 +0500

    Day 3 - Debian machine AD Join

---
+++ Day3/debian-to-ad.py
@@ -0,0 +1,23 @@
+import os
+from socket import socket
+from subprocess import STDOUT, check_call
+
+#installing the required packages
+check_call(['apt','install','-y','realmd','libnss-sss','libpam-sss','sssd','sssd-tools','adcli','samba-common-bin','oddjob','oddjob-mkhomedir','packagekit','zsh'], 
+    STDOUT=open(os.devnull, 'wb'), stderr=STDOUT)
+
+#User inputs
+hostname = input('Enter Hostname: ')
+domainName = input('Enter Domain Name: ')
+domainAdmin = input('Enter Domain Admin: ')
+
+#set hostname of the machine/system
+hostDomainName = hostname + '.' + domainName
+check_call(['hostnamectl', 'set-hostname', hostDomainName])
+
+#Join Domain and restart service
+check_call(['realm', 'join', '-v', '-U', domainAdmin, domainName])
+check_call(['systemctl', 'restart', 'sssd'])
+
+#check to see if everything is updated
+print('Updated Hostname: ' + socket.gethostname())
